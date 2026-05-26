842638f56faa63815cd770594f4ff376cedfec44
roney ruiz <roneyruizrojas@gmail.com>
2026-05-26 15:46:36 -0600

    feat: implement tspaths to clean imports

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,10 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@assets": "/src/assets",
+      "@sandbox": "/src/sandbox",
+    },
+  },
 });
